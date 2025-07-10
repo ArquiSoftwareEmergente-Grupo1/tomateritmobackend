54809ef42befc4e7b9d1a9ecde7ca81a8d48e1ef
GrandProgTest <u202124343@upc.edu.pe>
2025-07-10 09:17:20 -0500

    fix:maybe

--- src/main/java/com/tomateritmo/arqemergente/shared/config/WebConfig.java
+++ src/main/java/com/tomateritmo/arqemergente/shared/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", "https://chakys-frontend.web.app")
+                        .allowedOrigins("http://localhost:4200", "https://tomateritmo-frontend.web.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*");
             }
